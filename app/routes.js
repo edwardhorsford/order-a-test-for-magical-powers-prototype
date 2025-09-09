6a1303507ca784969a57e4fc55ab5a1d990fa01a
Ed Horsford <mail@edwardhorsford.com>
2025-09-09 14:57:02 +0000

    Add routing

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,20 @@ const express = require('express')
 
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.post('/magical-powers-answer', (req, res) => {
+
+  const hasSymptoms = req.session.data.hasSymptoms
+
+  if (hasSymptoms === 'Yes') {
+    res.redirect('/details')
+  } 
+  else if (hasSymptoms === 'No') {
+    res.redirect('/ineligible')
+  }
+  else {
+    res.redirect('/magical-powers')
+  }
+
+})
 
 module.exports = router
